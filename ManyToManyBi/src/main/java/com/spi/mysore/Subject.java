8a27430f058f47321e44d4b0eee6048670f9878a
BhargaviAasam <bhargaviaasam@gmail.com>
2018-07-12 19:09:21 +0530

    adding manytomany project

---
+++ ManyToManyBi/src/main/java/com/spi/mysore/Subject.java
@@ -0,0 +1,68 @@
+package com.spi.mysore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+	@Table(name = "SUBJECT")
+	public class Subject {
+	 
+	    @Id
+	    @GeneratedValue
+	    @Column(name = "SUBJECT_ID")
+	    private long id;
+	 
+	    @Column(name = "NAME")
+	    private String name;
+	     
+	     
+	    @ManyToMany(mappedBy="subjects")
+	    private List<Student> students = new ArrayList<Student>();
+	     
+	    public Subject(){
+	         
+	    }
+	     
+	    public Subject(String name){
+	        this.name = name;
+	    }
+	 
+	    public long getId() {
+	        return id;
+	    }
+	 
+	    public void setId(long id) {
+	        this.id = id;
+	    }
+	 
+	    public String getName() {
+	        return name;
+	    }
+	 
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	 
+	 
+	    public List<Student> getStudents() {
+	        return students;
+	    }
+	 
+	    public void setStudents(List<Student> students) {
+	        this.students = students;
+	    }
+
+		@Override
+		public String toString() {
+			return "Subject [id=" + id + ", name=" + name + ", students=" + students + "]";
+		}
+	 
+
+}
---
+++ ManyToManyUni/src/main/java/com/spi/mysore/HibernateMain.java
@@ -0,0 +1,39 @@
+package com.spi.mysore;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateMain {
+	 public static void main(String[] args) {
+		 Configuration config= new Configuration();
+	    	config.configure("hibernate.cfg.xml");
+	    	SessionFactory factory= config.buildSessionFactory();
+	        Session session = factory.openSession();
+	        Transaction txn=session.beginTransaction();
+		 
+	     Student student1=new Student("Bhargavi", "Reddy");
+	     Student student2=new Student("Keerthi", "Suresh");
+	        Subject subject1 = new Subject("Economics");
+	        Subject subject2 = new Subject("Politics");
+	        Subject subject3 = new Subject("Foreign Affairs");
+	 
+	        //Student1 have 3 subjects
+	        student1.getSubjects().add(subject1);
+	        student1.getSubjects().add(subject2);
+	        student1.getSubjects().add(subject3);
+	         
+	        //Student2 have 2 subjects
+	        student2.getSubjects().add(subject1);
+	        student2.getSubjects().add(subject2);
+	 
+	        session.save(student1);
+	        session.save(student2);
+	         
+	        txn.commit();
+	        session.close();  
+	    }
+
+}
